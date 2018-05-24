c83cdd2ace8bbaceaa329b146c98d18f4cfe5a35
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-3.local>
2018-05-24 13:29:01 -0600

    added drill 1

--- drills/version-1/car.js
+++ drills/version-1/car.js
@@ -1,28 +1,31 @@
-"use strict";
-exports.__esModule = true;
-var Car = /** @class */ (function () {
-    function Car(mpg) {
-        this.tripLog = [''];
-        this.gallons = 0;
-        this.milesDriven = 0;
-        this.mpg = mpg;
-        // this.tripLog.push(`${miles} miles`)
+class Car {
+    constructor (mpg) {
+        this.gallons = 0
+        this.mpg = mpg
+        this.milesDriven = 0
+        this.tripsArr = []
     }
-    Car.prototype.fill = function (gallonsOfGas) {
-        this.gallons += gallonsOfGas;
-    };
-    Car.prototype.drive = function (miles) {
-        var gasUsed = miles / this.mpg;
-        this.gallons -= gasUsed;
-        this.milesDriven += miles;
-        this.tripLog.push(miles + " miles");
-    };
-    Car.prototype.odometer = function () {
-        return this.milesDriven;
-    };
-    Car.prototype.trips = function () {
-        return this.tripLog;
-    };
-    return Car;
-}());
-exports.Car = Car;
+
+    fill (number) {
+        this.gallons += number
+    }
+
+    drive (distance) {
+        let usedGallons = distance / this.mpg
+        this.gallons -= usedGallons
+        this.milesDriven += distance
+        this.tripsArr.push(`${distance} miles`)
+    }
+
+    odometer () {
+        return this.milesDriven
+    }
+
+    trips (miles) {
+        return this.tripsArr
+    }
+}
+
+
+
+module.exports = Car
